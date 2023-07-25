c49885f545ff2bc2bd71d3accc097dc3388a679b
Yago Costa Ayala <77995791+YagoAyala@users.noreply.github.com>
2023-07-25 11:10:13 -0300

    feat: create all crud

--- server/src/controllers/portableDocumentFormat.controller.js
+++ server/src/controllers/portableDocumentFormat.controller.js
@@ -1,51 +1,79 @@
-const portableDocumentFormatService = require("../services/portableDocumentFormat.service")
+const CONST = require("../CONSTANTS.JS");
+const portableDocumentFormatService = require("../services/portableDocumentFormat.service");
 
 const createPdfController = async (req, res) => {
-    try {
-        const pdfData = req.body.newPdF
-        await portableDocumentFormatService.createPdfService(pdfData)
-        res.status(201).send({Success: "X"})
-    } catch (error) {
-        res.status(500).send({Error: "X", ErrorMessage: error.message})
-    }
-}
+  try {
+    const pdfData = req.body;
+    const { status } = CONST.CREATE_REQUEST;
+
+    const newEntity = await portableDocumentFormatService.createPdfService(pdfData);
+
+    res.status(status).json(newEntity);
+  } catch (error) {
+    res.status(500).send({ Error: "X", ErrorMessage: error.message });
+  }
+};
 
 const getAllPdfsController = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+  try {
+    const { status } = CONST.GET_ALL_REQUEST;
+
+    const result = await portableDocumentFormatService.getAllPdfsService();
+
+    res.status(status).send(result);
+  } catch (error) {
+    res.status(500).send({ Error: "X", ErrorMessage: error.message });
+  }
+};
 
 const getPdfByIdController = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+  try {
+    const { status } = CONST.DELETED_REQUEST;
+    const pdfId = req.params.id;
+
+    const result = await portableDocumentFormatService.getPdfByIdService(pdfId);
+
+    res.status(status).json(result);
+  } catch (error) {
+    res.status(500).send({ Error: "X", ErrorMessage: error.message });
+  }
+};
 
 const updatePdfController = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+  try {
+    const { status } = CONST.UPDATE_REQUEST;
+    const pdfData = req.body;
+    const pdfId = pdfData.id;
 
-const deletePdfController= async (req, res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+    delete pdfData.id;
+
+    const result = await portableDocumentFormatService.updatePdfService(
+      pdfId,
+      pdfData
+    );
+    res.status(status).json(result);
+  } catch (error) {
+    res.status(500).send({ Error: "X", ErrorMessage: error.message });
+  }
+};
+
+const deletePdfController = async (req, res) => {
+  try {
+    const { status, response } = CONST.DELETED_REQUEST;
+    const pdfId = req.params.id;
+
+    await portableDocumentFormatService.deletePdfService(pdfId);
+
+    res.status(status).json(response);
+  } catch (error) {
+    res.status(500).send({ Error: "X", ErrorMessage: error.message });
+  }
+};
 
 module.exports = {
-    createPdfController,
-    getAllPdfsController,
-    getPdfByIdController,
-    updatePdfController,
-    deletePdfController
-}
+  createPdfController,
+  getAllPdfsController,
+  getPdfByIdController,
+  updatePdfController,
+  deletePdfController,
+};
--- server/src/routes/portableDocumentFormat.routes.js
+++ server/src/routes/portableDocumentFormat.routes.js
@@ -3,5 +3,10 @@ const router = express.Router();
 const portableDocumentFormatController = require("../controllers/portableDocumentFormat.controller")
 
 router.post("/create", portableDocumentFormatController.createPdfController)
+router.get("/get_all", portableDocumentFormatController.getAllPdfsController)
+router.get("/get_by_id/:id", portableDocumentFormatController.getPdfByIdController)
+router.put("/update", portableDocumentFormatController.updatePdfController)
+router.delete("/delete/:id", portableDocumentFormatController.deletePdfController)
+
 
 module.exports = router
